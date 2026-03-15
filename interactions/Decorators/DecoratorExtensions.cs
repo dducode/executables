e8a5bace6241b43c3dce485fc845d904818f6a09
Denis Dmitriev <d.dmitriev@warpoint.ru>
2026-03-15 22:50:28 +0400

    fix

--- interactions/Decorators/DecoratorExtensions.cs
+++ interactions/Decorators/DecoratorExtensions.cs
@@ -7,6 +7,7 @@ public static class DecoratorExtensions {
 
   [Pure]
   public static Decorator<T1, T3> Compose<T1, T2, T3>(this Decorator<T1, T2> first, Decorator<T2, T3> second) {
+    first.ThrowIfNullReference();
     ExceptionsHelper.ThrowIfNull(second, nameof(second));
     return new CompositeDecorator<T1, T2, T3>(first, second);
   }
