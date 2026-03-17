9446a2f357cc7f7fb5150221e0a676c25110c69d
Denis Dmitriev <d.dmitriev@warpoint.ru>
2026-03-17 14:54:51 +0400

    async fork tests added

---
+++ interactions.tests/Executables/AsyncForkExecutableTest.cs
@@ -0,0 +1,104 @@
+using Interactions.Core;
+using Interactions.Core.Executables;
+using Interactions.Executables;
+using JetBrains.Annotations;
+
+namespace Interactions.Tests.Executables;
+
+[TestSubject(typeof(AsyncForkExecutable<,,>))]
+public class AsyncForkExecutableTest {
+
+  [Fact]
+  public async Task SimpleFork() {
+    IAsyncQuery<int, string> query = AsyncExecutable
+      .Identity<int>()
+      .Fork(
+        async (x, token) => {
+          await Task.Delay(10, token);
+          return x * 2;
+        },
+        async (x, token) => {
+          await Task.Delay(50, token);
+          return x + 10;
+        })
+      .Merge((a, b, _) => new ValueTask<string>($"{a}:{b}"))
+      .AsQuery();
+
+    Assert.Equal("2:11", await query.Send(1));
+  }
+
+  [Fact]
+  public async Task NestedFork() {
+    IAsyncExecutable<int, int> branchSum = AsyncExecutable
+      .Identity<int>()
+      .Fork(
+        async (x, token) => {
+          await Task.Delay(10, token);
+          return x * 2;
+        },
+        async (x, token) => {
+          await Task.Delay(50, token);
+          return x + 10;
+        })
+      .Merge((a, b, _) => new ValueTask<int>(a + b));
+
+    IAsyncExecutable<int, int> branchDiff = AsyncExecutable
+      .Identity<int>()
+      .Fork(
+        async (x, token) => {
+          await Task.Delay(50, token);
+          return x + 10;
+        },
+        async (x, token) => {
+          await Task.Delay(10, token);
+          return x * 2;
+        })
+      .Merge((a, b, _) => new ValueTask<int>(a - b));
+
+    IAsyncQuery<int, string> query = AsyncExecutable
+      .Identity<int>()
+      .Fork(branchSum, branchDiff)
+      .Merge((a, b, _) => new ValueTask<string>($"{a}:{b}"))
+      .AsQuery();
+
+    Assert.Equal("13:9", await query.Send(1));
+  }
+
+  [Fact]
+  public async Task ThrowExceptionWhenAnyFaulted() {
+    IAsyncQuery<int, string> firstFaulted = AsyncExecutable
+      .Identity<int>()
+      .Fork(
+        ValueTask<int> (_, _) => throw new InvalidOperationException(),
+        async (x, token) => {
+          await Task.Delay(10, token);
+          return x + 2;
+        })
+      .Merge((a, b, _) => new ValueTask<string>($"{a}:{b}"))
+      .AsQuery();
+
+    IAsyncQuery<int, string> secondFaulted = AsyncExecutable
+      .Identity<int>()
+      .Fork(
+        async (x, token) => {
+          await Task.Delay(10, token);
+          return x + 2;
+        },
+        ValueTask<int> (_, _) => throw new InvalidOperationException())
+      .Merge((a, b, _) => new ValueTask<string>($"{a}:{b}"))
+      .AsQuery();
+
+    IAsyncQuery<int, string> bothFaulted = AsyncExecutable
+      .Identity<int>()
+      .Fork(
+        ValueTask<int> (_, _) => throw new InvalidOperationException(),
+        ValueTask<int> (_, _) => throw new InvalidOperationException())
+      .Merge((a, b, _) => new ValueTask<string>($"{a}:{b}"))
+      .AsQuery();
+
+    await Assert.ThrowsAsync<InvalidOperationException>(async () => await firstFaulted.Send(0));
+    await Assert.ThrowsAsync<InvalidOperationException>(async () => await secondFaulted.Send(0));
+    await Assert.ThrowsAsync<InvalidOperationException>(async () => await bothFaulted.Send(0));
+  }
+
+}
